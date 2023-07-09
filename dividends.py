5afefe1bd2dcfc1e5088c8e611b725382311a9ca
PyMyCode <pymycode@protonmail.com>
2023-07-09 21:12:26 +0200

    added dividend graph

--- dividends.py
+++ dividends.py
@@ -1,15 +1,22 @@
+from math import nan
 import streamlit as st
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.style as sty
 
+# Page configs
+st.set_page_config(layout="wide") # streamlit pag uses compltet width
+
+# containers
+dividend_history_container = st.container()
+
 class Dividends:
     # Constructor
     def __init__(self, stock):
         
+        # getting stock smbol as str
         self.stock = stock
-
         self.stock_list = self.stock.split()
         
         # Only 1 Input
@@ -18,14 +25,19 @@ class Dividends:
             exit()
         
         if self.stock:
-            self.t = self._extraction()
+            self.t = self._extraction_ticker()
             # splitting stock names
         else:
             exit()
-    
-        self._dividends_info()
-    
-    def _extraction(self):
+        # calling method to extract data
+        df = self._extraction_data()
+
+        # calling method create dividend history graph
+        self._dividend_history_graph(df)
+
+
+    def _extraction_ticker(self):
+
         "Trying to extract data from yfinance"
         try:
             t = yf.Ticker(self.stock)
@@ -36,45 +48,76 @@ class Dividends:
         except:
             print("\nInvalid Symbol\n")
             exit()
-    
-    def _dividends_info(self):
 
-        df = self.t.dividends
+    def _extraction_data(self):
         
-        # grouping by year
-        df_yearly = df.groupby(df.index.year).agg('sum')
+        # historical share price
+        try:
+            df_sp = yf.download(
+            tickers = self.stock,
+            period = "max",
+            interval = "1d",
+            group_by = 'ticker',
+            prepost = False,
+            repair = True
+            )
+        except:
+            st.write("could not historical share price data")
+            exit()
 
-        # create graph
-        self._dividend_rate_graph(df_yearly)
-
-        print(df_yearly)
-
-    def _dividend_rate_graph(self, df):
+        # dividend data
+        try:
+            df_div = self.t.dividends
+        except:
+            st.write("dividend data could not be extracted")
+            exit()
         
-        #TODO: SUperimpose a scatterplot on a barplot
+        # removing localize times
+        df_div = df_div.tz_localize(None)
+
+        # joining dataframes
+        df = df_sp.join(df_div)
+        
+        print("Dividend Dates")
+        print(df[df["Dividends"].notnull()])
+
+        return df
+
+    def _dividend_history_graph(self, df):
 
         # setting the graph style
         sty.use(['dark_background'])
         st.set_option('deprecation.showPyplotGlobalUse', False)
 
         # setting overall font size
-        plt.rcParams['font.size'] = 12
+        plt.rcParams['font.size'] = 8
 
         # creating the subplot
-        fig, ax = plt.subplots(figsize=(1000 / 100, 6))
+        fig, ax = plt.subplots(figsize=(20, 6))
+        #fig, ax = plt.subplots()
 
-        # creating a barplot
-        ax.bar(df.index, df.values)
+        # Plot the line chart
+        ax.plot(df.index, df.Close, label='Line Chart')
+
+        # Set labels and title
         ax.set_xlabel('Year')
         ax.set_ylabel('Dividend Rate')
-        ax.set_title('Yearly Dividend Rate')
-        
-        # showing all years with dividends
-        plt.xticks(df.index, rotation='vertical')
 
-        # annotations (show value on the )
-        ax.bar_label(ax.containers[0])
+        # Collecting dividend annotations
+        annotations = df[df['Dividends'].notnull()]
 
-        ax.legend()
+        # Add annotations based on the 'label' column
+        for i, row in annotations.iterrows():
+            x = i
+            y = row["Close"]
+            # adding date of distr.
+            label = str(round(row['Dividends'],2)) + "\n" + i.strftime(('%m/%Y'))
+            # adding annotation
+            ax.annotate(label, (x, y), textcoords="offset points", xytext=(0,50), ha='center',
+                        arrowprops=dict(arrowstyle = '-', connectionstyle = 'arc3',facecolor='red'))
 
-        st.pyplot(fig)
+        with dividend_history_container:
+            
+            st.write("Dividend History")
+            # Display the plot
+            st.pyplot(fig)
